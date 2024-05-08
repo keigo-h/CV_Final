fbf3c679c67936b5a6dc6a436f21a43ba70c655c
Keigo Hachisuka <keigoh@keigos-mbp.devices.brown.edu>
2024-05-08 16:19:20 -0400

    updates to get text covered

--- code_3/handle_input.py
+++ code_3/handle_input.py
@@ -4,6 +4,9 @@ from scipy.stats import mode
 
 def process_input_img(img_path):
     img = cv2.imread(img_path)
+    orig_img = img.copy()
+    cv2.imshow('orig', orig_img)
+    cv2.waitKey()
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # gray_img = cv2.GaussianBlur(gray_img, (11,11), 5)
     # gray_img = cv2.medianBlur(gray_img, 5)
@@ -12,7 +15,7 @@ def process_input_img(img_path):
     horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
     dilation = cv2.dilate(thresh1, horizontal_kernel, iterations=1)
     horizontal_contours, _ = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    return horizontal_contours
+    return img, orig_img, horizontal_contours
 
 def get_char_cors(horizontal_contours):
     cors = []
@@ -36,16 +39,37 @@ def calculate_threshold(cords):
     return threshold
 
 def generate_text(img, word_locs):
+    phrase = ""
     for left, right, top, bottom in word_locs:
-       pass 
-    pass
+       text = extract_text(img[bottom:top, left:right], model=None)
+       phrase += f'{text} '
+    return phrase[:-1]
 
 def extract_text(word_img, model):
-    pass
+    return "HELLO"
 
 def translate_phrase(word, lang):
-    pass
+    return ""
 
+def get_word_loc(img, start, end, min_height, max_height):
+    left, right, top, bottom = 0, 0, 0, 0
+    if start == end:
+        left = start[0]
+        right = end[0] + end[2]
+        bottom = start[1]
+        top = start[1] + start[3]
+        cv2.rectangle(img, (start[0], start[1]), (start[0] + start[2], start[1]+ start[3]), (255, 0, 0), 2)
+        cv2.imshow('Words', img)
+        cv2.waitKey()
+    else:
+        left = start[0]
+        right = end[0] + end[2]
+        bottom = min_height
+        top = max_height
+        cv2.rectangle(img, (start[0], min_height), (end[0] + end[2], max_height), (255, 0, 0), 2)
+        cv2.imshow('Words', img)
+        cv2.waitKey()
+    return left, right, top, bottom
 
 def get_word_cords(img, cors, threshold):
     i = 0
@@ -63,61 +87,46 @@ def get_word_cords(img, cors, threshold):
             min_height = min(min_height, y_p)
             i += 1
         else:
-            if start == end:
-                cv2.rectangle(img, (start[0], start[1]), (start[0] + start[2], start[1]+ start[3]), (255, 0, 0), 2)
-            else:
-                cv2.rectangle(img, (start[0], min_height), (end[0] + end[2], max_height), (255, 0, 0), 2)
-            word_locs.append((start, end, max_height, min_height))
+            word_locs.append(get_word_loc(img, start, end, min_height, max_height))
             start = cors[i + 1]
             end = cors[i + 1]
             max_height = start[1] + start[3]
             min_height = start[1]
             i += 1
-    if start == end:
-        cv2.rectangle(img, (start[0], start[1]), (start[0] + start[2], start[1]+ start[3]), (255, 0, 0), 2)
-    else:
-        cv2.rectangle(img, (start[0], min_height), (end[0] + end[2], max_height), (255, 0, 0), 2)
-    word_locs.append((start, end, max_height, min_height))
+    word_locs.append(get_word_loc(img, start, end, min_height, max_height))
     return word_locs
 
 def cover_text(img, word_locs):
-    bottom, top, left, right = 0
+    bottom, top, left, right = np.inf, 0, np.inf, 0
     for start, end, maxx, minn in word_locs:
         bottom = min(bottom, minn)
         top = max(top, maxx)
-        left = min(left, start[0])
-        right = max(right, end[0] + end[2])
+        left = min(left, start)
+        right = max(right, end)
 
 
     avg_color = np.mean(img[bottom:top, left:right])
     img[bottom:top, left:right] = avg_color
     cv2.imshow('Text Covered', img)
     cv2.waitKey()
-    return img
+    return img, top, left
 
-def write_text(img, text, x,y, font_size):
-    texted_image =cv2.putText(img=img, text=text, org=(x, y),fontFace=3, fontScale=3, color=(0,0,0), thickness=5)
-
-def preProcessing(myImage):
-    grayImg = cv2.cvtColor(myImage, cv2.COLOR_BGR2GRAY)
+def write_text(img, text, x,y, font_size = 3):
+    return cv2.putText(img=img, text=text, org=(x, y),fontFace=3, fontScale=3, color=(0,0,0), thickness=5)
     
 
-    im2 = myImage.copy()
-    
-
-    
-
-
-    # threshold = 150
-    
-
-
-
-    cv2.imshow('After threshold', im2)
+def translate_text(img_path):
+    img, orig_img, horz_cont = process_input_img(img_path)
+    char_cors = get_char_cors(horz_cont)
+    word_locs = get_word_cords(orig_img, char_cors, calculate_threshold(char_cors))
+    pharse = generate_text(orig_img, word_locs)
+    translated_text = translate_phrase(pharse, "german")
+    cover_img, bottom, left = cover_text(orig_img, word_locs)
+    final_img = write_text(cover_img, translated_text, left, bottom)
+    cv2.imshow('Translated Image', final_img)
     cv2.waitKey()
 
-    return im2
-
-preProcessing(cv2.imread("/Users/keigoh/Desktop/CS1430_Attempt_3/finalproject-keigo-h/code_3/IMG_6588.png"))
-preProcessing(cv2.imread("/Users/keigoh/Desktop/CS1430_Attempt_3/finalproject-keigo-h/code_3/IMG_6587.png"))
-preProcessing(cv2.imread("/Users/keigoh/Desktop/CS1430_Attempt_3/finalproject-keigo-h/code_3/IMG_6586.png"))
+    
+translate_text("/Users/keigoh/Desktop/CS1430_Attempt_3/CV_Final/code_3/IMG_6586.png")
+# translate_text("/Users/keigoh/Desktop/CS1430_Attempt_3/CV_Final/code_3/IMG_6587.png")
+# translate_text("/Users/keigoh/Desktop/CS1430_Attempt_3/CV_Final/code_3/IMG_6588.png")
