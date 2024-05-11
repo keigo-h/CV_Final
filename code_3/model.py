7294cb2fbe1d29fba7ac778f3889f549166f3c8c
Keigo Hachisuka <keigoh@keigos-mbp.mynetworksettings.com>
2024-05-11 16:48:26 -0400

    bill multi line working

--- code_3/model.py
+++ code_3/model.py
@@ -1,15 +1,7 @@
-import tensorflow as tf
-import random
-import keras
-import csv
-import cv2
-from matplotlib import pyplot as plt
 import numpy as np
-from keras.layers import Dense, LSTM, Reshape, BatchNormalization, Input, Conv2D, MaxPool2D, Lambda, Bidirectional,Dropout, ReLU, Activation
+from keras.layers import Dense, LSTM, Reshape, BatchNormalization, Input, Conv2D, MaxPool2D, Lambda, Bidirectional
 from keras.models import Model
-from keras.activations import relu, sigmoid, softmax
-import keras.backend as K
-from keras.utils import to_categorical
+import keras.backend
 from keras.callbacks import ModelCheckpoint, CSVLogger
 from preprocess import HandleData
 
@@ -39,7 +31,6 @@ def create_model():
     model = MaxPool2D(pool_size=(1,2), strides=(1,2), padding='valid')(model)
 
     model = Reshape(target_shape=(32,256))(model)
-    # model = Reshape(target_shape=(8,1024))(model)
 
     model = Bidirectional(LSTM(256, return_sequences=True, dropout = 0.2))(model)
     model = Bidirectional(LSTM(256, return_sequences=True, dropout = 0.2))(model)
@@ -47,89 +38,17 @@ def create_model():
     outputs = Dense(len(char_list)+1, activation = 'softmax')(model)
 
     the_model = Model(inputs, outputs)
-    the_model.summary()
     return the_model,outputs,inputs
-
-def create_model_3():
-        # input with shape of height=32 and width=128 
-    inputs = Input(shape=(32,128,1))
-
-    # convolution layer with kernel size (3,3)
-    conv_1 = Conv2D(64, (3,3), activation = 'relu', padding='same')(inputs)
-    # poolig layer with kernel size (2,2)
-    pool_1 = MaxPool2D(pool_size=(2, 2), strides=2)(conv_1)
-
-    conv_2 = Conv2D(128, (3,3), activation = 'relu', padding='same')(pool_1)
-    pool_2 = MaxPool2D(pool_size=(2, 2), strides=2)(conv_2)
-
-    conv_3 = Conv2D(256, (3,3), activation = 'relu', padding='same')(pool_2)
-
-    conv_4 = Conv2D(256, (3,3), activation = 'relu', padding='same')(conv_3)
-    # poolig layer with kernel size (2,1)
-    pool_4 = MaxPool2D(pool_size=(2, 1))(conv_4)
-
-    conv_5 = Conv2D(512, (3,3), activation = 'relu', padding='same')(pool_4)
-    # Batch normalization layer
-    batch_norm_5 = BatchNormalization()(conv_5)
-
-    conv_6 = Conv2D(512, (3,3), activation = 'relu', padding='same')(batch_norm_5)
-    batch_norm_6 = BatchNormalization()(conv_6)
-    pool_6 = MaxPool2D(pool_size=(2, 1))(batch_norm_6)
-
-    conv_7 = Conv2D(512, (2,2), activation = 'relu')(pool_6)
-
-    squeezed = Lambda(lambda x: K.squeeze(x, 1))(conv_7)
-
-    # bidirectional LSTM layers with units=128
-    blstm_1 = Bidirectional(LSTM(256, return_sequences=True, dropout = 0.2))(squeezed)
-    blstm_2 = Bidirectional(LSTM(256, return_sequences=True, dropout = 0.2))(blstm_1)
-
-    outputs = Dense(len(char_list)+1, activation = 'softmax')(blstm_2)
-
-    # model to be used at test time
-    act_model = Model(inputs, outputs)
-    act_model.summary()
-    return act_model,outputs,inputs
-
-def create_model_2():
-    inputs = Input(shape=(32,128,1))
-        ## Convolutional layers
-    # layer 1 
-    conv_1 = Conv2D(64, 3, strides=1, padding="same", kernel_initializer="he_normal", activation="relu", name="conv_1")(inputs)
-    # layer 2
-    conv_2 = Conv2D(32, 3, strides=1, padding="same", kernel_initializer="he_normal", activation="relu", name="conv_2")(conv_1)
-    max_pool_1 = MaxPool2D(pool_size=(2, 2), strides=(2, 2))(conv_2)
-    # layer 3
-    conv_3 = Conv2D(64, 3, strides=1, padding='same', activation='relu', kernel_initializer='he_normal', name="conv_3")(max_pool_1)
-    conv_4 = Conv2D(32, 3, strides=1, padding='same', activation='relu', kernel_initializer='he_normal', name="conv_4")(conv_3)
-    max_pool_2 = MaxPool2D(pool_size=(2, 2), strides=(2, 2))(conv_4)
-
-
-
-    ### Encoding 
-    reshape = Reshape(target_shape=((128//4), (32//4)*32), name="reshape_layer")(max_pool_2)
-    dense_encoding = Dense(64, kernel_initializer="he_normal", activation="relu", name="enconding_dense")(reshape)
-    dense_encoding_2 = Dense(64, kernel_initializer="he_normal", activation="relu", name="enconding_dense_2")(dense_encoding)
-    dropout = Dropout(0.4)(dense_encoding_2)
-
-    # Decoder
-    lstm_1 = Bidirectional(LSTM(128, return_sequences=True, dropout=0.25), name="bidirectional_lstm_1")(dropout)
-    lstm_2 = Bidirectional(LSTM(64, return_sequences=True, dropout=0.25), name="bidirectional_lstm_2")(lstm_1)
-    outputs = Dense(len(char_list)+1, activation = 'softmax')(lstm_2)
-
-    act_model = Model(inputs, outputs)
-    act_model.summary()
-    return act_model,outputs,inputs
-
 
 def ctc_loss_func(args):
     y_pred, labels, input_length, label_length = args
-    return K.ctc_batch_cost(labels, y_pred, input_length, label_length)
+    return keras.backend.ctc_batch_cost(labels, y_pred, input_length, label_length)
 
 def gen_model(hD: HandleData):
     train_img, train_label, train_label_len, train_inp_len = hD.process_train()
     val_img, val_label, val_label_len, val_inp_len = hD.process_val()
-    test_model, out, inp = create_model()
+    the_model, out, inp = create_model()
+    the_model.summary()
     the_labels = Input(shape=[hD.max_len], dtype='float32')
     input_length = Input(shape=[1], dtype='int64')
     label_length = Input(shape=[1], dtype='int64')
@@ -156,9 +75,6 @@ def gen_model(hD: HandleData):
                     callbacks=callbacks_list)
 
     model.save(filepath='./model.h5', overwrite=True, include_optimizer=True)
-
-    with open("output.txt", 'w') as output_file:
-        output_file.write(history.history)
 
 def main():
     # create_model()
--- code_3/preprocess.py
+++ code_3/preprocess.py
@@ -1,18 +1,7 @@
 import tensorflow as tf
 import random
-import keras
-import csv
 import cv2
-from matplotlib import pyplot as plt
 import numpy as np
-from keras.layers import Dense, LSTM, Reshape, BatchNormalization, Input, Conv2D, MaxPool2D, Lambda, Bidirectional,Dropout
-from keras.models import Model
-from keras.activations import relu, sigmoid, softmax
-import keras.backend as K
-from keras.utils import to_categorical
-from keras.callbacks import ModelCheckpoint
-
-MAX_LEN = 0
 
 class HandleData:
     def __init__(self, img_size) -> None:
@@ -42,11 +31,6 @@ class HandleData:
                 line = file.readline()
         
         assert len(self.img_paths) == len(self.label_paths)
-        # temp = list(zip(self.img_paths, self.label_paths))
-        # random.shuffle(temp)
-        # self.img_paths, self.label_paths = zip(*temp)
-        # self.img_paths = self.img_paths[:5265]
-        # self.label_paths = self.label_paths[:5265]
         idx = int(len(self.img_paths) * 0.95)
         self.max_len = max_len
         self.train_img = self.img_paths[:idx]
@@ -87,17 +71,15 @@ class HandleData:
             w, h = img.shape
             
         if h > 128 or w > 32:
-            dim = (128,32)
-            img = cv2.resize(img, dim)
+            shape = (128,32)
+            img = cv2.resize(img, shape)
 
         img = np.expand_dims(img, -1)
-        # img = cv2.transpose(img)
         img = img / 255 - 0.5
 
         return img
     
     def process_train(self):
-        print("process train")
         train_imgs = []
         train_labels = []
         train_labels_len = []
@@ -112,7 +94,6 @@ class HandleData:
         return np.asarray(train_imgs), np.asarray(train_labels), np.asarray(train_labels_len), np.asarray(train_input_len)
 
     def process_val(self):
-        print("process val")
         val_imgs = []
         val_labels = []
         val_labels_len = []
